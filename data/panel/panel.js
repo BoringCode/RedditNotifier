5a9b722fc92821e62d0f5253e5c6fcf596cc76de
BoringCode <rosenfeld.bradley@gmail.com>
2015-06-11 23:56:00 -0400

    Created panel to display current messages

--- data/panel/panel.js
+++ data/panel/panel.js
@@ -1,5 +1,52 @@
-"use strict";
+/* Panel.js
+ * Displays unread messages in the addon panel
+ */
 
-self.port.on("show", function(data) {
-   console.log(data);
-});
+(function() {
+   "use strict";
+
+   var messageList = document.querySelector(".message-list");
+   var items = [];
+   var unread = {};
+
+   //Function called when user clicks on message
+   var open = function(obj) {
+      self.port.emit("open", obj);
+   }
+
+   var resetMessageList = function() {
+      //Loop through and properly delete each element
+      for (var i = 0; i < items.length; i++) {
+         items[i].removeEventListener("click", itemClick);
+         messageList.removeChild(items[i]);
+      }
+      items = [];
+      messageList.innerHTML = "";
+   }
+
+   var itemClick = function(e) {
+      open(unread[this.dataset.key]);
+      e.preventDefault();
+      return false;
+   }
+
+   //Fills message list when panel is shown
+   self.port.on("show", function(data) {
+      unread = data;
+      var obj;
+      //Reset message list
+      resetMessageList();
+      //Loop through messages passed in
+      for (var prop in unread) {
+         obj = unread[prop];
+         //Create element and add it to the message list
+         var item = document.createElement("li");
+         item.setAttribute("data-key", prop);
+         items.push(item);
+         item.innerHTML = obj.message;
+         messageList.appendChild(item);
+         //On click, open the the unread message
+         item.addEventListener("click", itemClick);
+      }
+   });
+})();
