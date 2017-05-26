c4c9fba1edf9de5e5ba5ee0d365d7b37516e3151
Bradley Rosenfeld <thatguy@bradleyrosenfeld.com>
2017-05-25 22:29:47 -0400

    Panel displays notifications to user

--- data/panel/panel.js
+++ data/panel/panel.js
@@ -1,55 +1,39 @@
-/* Panel.js
+/* 
+ * Panel.js
  * Displays unread messages in the addon panel
  */
-console.log("HELLO");
 
-(function() {
-   "use strict";
+"use strict";
 
-   console.log(config);
+// Get messages from background script
+browser.runtime.sendMessage({ type: "getMessages" }, function(response) {
+   if (typeof(response) !== "object") return;
 
    var messageList = document.querySelector(".message-list");
-   var items = [];
-   var unread = {};
+   var items = [], i, item; 
 
-   //Function called when user clicks on message
-   var open = function(obj) {
-      self.port.emit("open", obj);
+   // Create elements that users can click
+   for (i = 0; i < response.length; i++) {
+      if (!("action" in response[i])) continue;
+      item = document.createElement("li");
+      item.setAttribute("data-action", response[i]["action"]);
+      items.push(item);
+      item.textContent = response[i]["message"];
+      messageList.appendChild(item);
+      //On click, open the the unread message
+      item.addEventListener("click", itemClick);
    }
+})
 
-   var resetMessageList = function() {
-      //Loop through and properly delete each element
-      for (var i = 0; i < items.length; i++) {
-         items[i].removeEventListener("click", itemClick);
-         messageList.removeChild(items[i]);
-      }
-      items = [];
-      messageList.innerHTML = "";
-   }
-
-   var itemClick = function(e) {
-      open(unread[this.dataset.key]);
-      e.preventDefault();
-      return false;
-   }
-
-   //Fills message list when panel is shown
-   /*self.port.on("show", function(data) {
-      unread = data;
-      var obj;
-      //Reset message list
-      resetMessageList();
-      //Loop through messages passed in
-      for (var prop in unread) {
-         obj = unread[prop];
-         //Create element and add it to the message list
-         var item = document.createElement("li");
-         item.setAttribute("data-key", prop);
-         items.push(item);
-         item.textContent = obj.message;
-         messageList.appendChild(item);
-         //On click, open the the unread message
-         item.addEventListener("click", itemClick);
-      }
-   });*/
-})();
+// Send message to main background script to update the things
+var itemClick = function(e) {
+   browser.runtime.sendMessage({
+      type: "openTab",
+      action: this.dataset.action
+   }, function(response) {
+      // Close popup
+      window.close();
+   })
+   e.preventDefault();
+   return false;
+}
